bec040b3e207e2b9f686ede415ef984a935ce71f
ambrossRake <ijohnson2013301@gmail.com>
2021-03-30 05:50:59 -0700

    🎨 - added color options

--- src/display.py
+++ src/display.py
@@ -12,12 +12,14 @@ class Display:
     def __init__(self):
         self.pixels = []
         self.window = tkinter.Tk()
-        self.scale = 16
+        self.scale = 8
         self.width = 64*self.scale
         self.height = 32*self.scale
+        self.foregroundColor = '#000'
+        self.backgroundColor = '#fff'
         self.window.geometry = ("%dx%d"%(self.width,self.height))
-        self.window.config(bg='#000')
-        self.canvas = tkinter.Canvas(self.window, bg="#000", height = self.height, width = self.width)
+        self.window.config(bg=self.backgroundColor)
+        self.canvas = tkinter.Canvas(self.window, bg=self.backgroundColor, height = self.height, width = self.width)
         self.canvas.pack()
         for y in range(self.height):
             row = []
@@ -29,15 +31,15 @@ class Display:
         self.window.title(title)
     def clear(self):
         for pixel in self.pixels:
-            canvas.delete(pixel[0])
+            self.canvas.delete(pixel[0])
 
     def set(self, row, col, value):
         pixel = self.pixels[row][col]
         if(value == 0):
             pixel[1] = 0
-            self.canvas.itemconfig(pixel[0], fill='black')
+            self.canvas.itemconfig(pixel[0], fill=self.backgroundColor)
         else:
-            self.canvas.itemconfig(pixel[0], fill='white')
+            self.canvas.itemconfig(pixel[0], fill=self.foregroundColor)
             self.pixels[row][col][1] = 1
 
     def get(self, row, col):
