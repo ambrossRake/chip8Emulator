f725b07caa0fa2109dd36cd4f63289e5662ee761
ambrossRake <ijohnson2013301@gmail.com>
2021-04-08 23:24:06 -0700

    Added Key Listener

--- src/keyboard.py
+++ src/keyboard.py
@@ -1,11 +1,19 @@
-import pynput as in
+import pynput
 
 class Keyboard:
 
     def __init__(self):
-        self.keyListener = in.keyboard.Listener(on_press=self.onPress,
-         on_release=self.onRelease)
+        self.__keyListener = pynput.keyboard.Listener(on_press=self.__onPress,
+         on_release=self.__onRelease)
+        self.__keysDown = set()
 
-    def onPress(self, key):
+        self.__keyListener.start()
 
-    def onRelease(self, key):
+    def __onPress(self, key):
+        self.__keysDown.add(key)
+
+    def __onRelease(self, key):
+        self.__keysDown.discard(key)
+
+    def isKeyDown(self, key):
+        return pynput.keyboard.KeyCode.from_char(key) in self.__keysDown
